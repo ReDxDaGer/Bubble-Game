9f3d902ba472175df243c4e5da96e3b3df2dda30
ReDxDaGer <yashmehrotra45@gmail.com>
2023-10-23 15:08:38 +0530

    Updated timer

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-var clutter = "";
-for(var i = 0; i <= 100; i++){
-    clutter += `<div class="bubble">5</div>`;
+function bubbleMaker(){
+    var clutter = "";
+    for(var i = 0; i <= 104; i++){
+        var num = Math.floor(Math.random()*10)
+        clutter += `<div class="bubble">${num}</div>`;
+    }
+    document.querySelector("#Pbtm").innerHTML = clutter;
 }
-document.querySelector("#Pbtm").innerHTML = clutter;
+var timer = 60;
+function setTimer(){
+    var timeOut  = setInterval(function(){
+        if(timer>0){
+            timer--;
+            document.querySelector("#timer").textContent = timer;
+        }
+        else{
+            clearTimeout(timeOut)
+        }
+    },1000)
+}
+setTimer();
+bubbleMaker();
