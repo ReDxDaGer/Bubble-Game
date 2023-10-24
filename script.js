14be22205b2a0673f1259ad43d36f99fe1c9a86a
ReDxDaGer <yashmehrotra45@gmail.com>
2023-10-24 17:44:00 +0530

    Updated JS

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var timer = 60;
+var score = 0;
+
+
+function hitval(){
+    var rn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = rn;
+}
 function bubbleMaker(){
     var clutter = "";
     for(var i = 0; i <= 104; i++){
@@ -6,7 +14,6 @@ function bubbleMaker(){
     }
     document.querySelector("#Pbtm").innerHTML = clutter;
 }
-var timer = 60;
 function setTimer(){
     var timeOut  = setInterval(function(){
         if(timer>0){
@@ -18,5 +25,6 @@ function setTimer(){
         }
     },1000)
 }
+hitval();
 setTimer();
 bubbleMaker();
